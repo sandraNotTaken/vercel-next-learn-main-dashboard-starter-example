10ebbc85438fd7a9bf2b4590b4b53e9c304afc5c
sandraNotTaken <155741111+sandraNotTaken@users.noreply.github.com>
2026-07-07 13:40:56 +0100

    adding the UI skeleton for the dashboard page and updating the data fetching logic to include a 3-second delay for demonstration purposes.

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,11 +1,11 @@
 import { GlobeAltIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
 
-export default function AcmeLogo() {
+export default function AcmeLogo({ className }: { className?: string }) {
+  const combined = `${lusitana.className} ${className ?? ''} flex flex-row items-center leading-none text-white`;
+
   return (
-    <div
-      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
-    >
+    <div className={combined}>
       <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
       <p className="text-[44px]">Acme</p>
     </div>
