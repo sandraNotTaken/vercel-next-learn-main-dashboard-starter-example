846277d1d6d648f0fa62ce8aaa0b9d47b02b1b62
sandraNotTaken <155741111+sandraNotTaken@users.noreply.github.com>
2026-07-09 10:41:35 +0100

    mutated data (commit): 5 files changed, 19 insertions(+), 11 deletions(-)

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -2,6 +2,8 @@
 
 import postgres from 'postgres';
 import { z } from 'zod';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 
 const sql = postgres(process.env.DATABASE_URL!, { ssl: 'require' });
 
@@ -13,6 +15,26 @@ const FormSchema = z.object({
     date: z.string(),
 });
 
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function updateInvoice(id: string, formData: FormData) {
+    const { customerId, amount, status } = UpdateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+    const amountInCents = amount * 100;
+
+    await sql`
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+    `;
+
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+}
+
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
  
 export async function createInvoice(formData: FormData) {
@@ -23,4 +45,17 @@ export async function createInvoice(formData: FormData) {
   });
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0]; // Get current date in YYYY-MM-DD format
+
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string) {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath('/dashboard/invoices');
 }
